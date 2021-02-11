e68f7a7b0ceedc2725090e10e60895d0e074b8f5
hisho <hisho.web@gmail.com>
2021-02-12 04:17:21 +0900

    ✅ media test

--- test/media.test.ts
+++ test/media.test.ts
@@ -1,10 +1,31 @@
 import {Media} from "../src";
 
 describe('Media', (): void => {
-  describe('constructor', (): void => {
-    test('create instance', (): void => {
-      const media = new Media();
-      expect(media).toBeDefined();
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }))
     });
   });
+
+  describe('constructor', (): void => {
+    test('create instance', (): void => {
+      const media = new Media(`(min-width: ${800 / 16}em)`);
+      expect(media).toBeDefined();
+      expect(media.on).toBeDefined();
+      expect(media.register).toBeDefined();
+      expect(media.unregister).toBeDefined();
+      expect(media.isMatch).toBeDefined();
+    });
+  });
+
 })
